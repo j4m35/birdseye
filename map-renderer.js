31334d48852c5c57847ca7e1e796f3b1da55a29a
James Chew <james05@gmail.com>
2026-06-01 14:11:57 +0800

    Fix basemap

--- map-renderer.js
+++ map-renderer.js
@@ -25,12 +25,11 @@ const MapRenderer = (function() {
     // Initialize Leaflet map with CartoDB Voyager tiles (free, no API key)
     map = L.map(mapContainerId).setView([5, 107], 4);
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/voyager_all/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       subdomains: 'abcd',
       maxZoom: 19,
-      minZoom: 2,
-      detectRetina: true // Leaflet will handle high-res screens automatically and safely
+      minZoom: 2
     }).addTo(map);
 
     // Enable double-click zoom but handle single clicks for markers
